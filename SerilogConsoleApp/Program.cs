96cb844a2e3032f0754348803fff2c63d3e9d81e
Feng Lv <75422919+IServiceStudio@users.noreply.github.com>
2021-01-01 20:07:40 +0800

    Serilog

---
+++ SerilogConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using Serilog;
+
+namespace SerilogConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        private static void HelloSerilog()
+        {
+            using var log =
+                   //构建一个日志记录管道
+                   new LoggerConfiguration()
+                   //将控制器添加到日志记录管道中
+                   .WriteTo.Console()
+                   //创建日志对象
+                   .CreateLogger();
+            log.Information("Hello,Serilog!");
+            log.Warning("Goodbye,Serilog.");
+
+            var itemNumber = 10;
+            var itemCount = 999;
+            log.Debug("Processing item {ItemNumber} of {ItemCount}", itemNumber, itemCount);
+
+            var user = new { Name = "Nick", Id = "nblumhardt" };
+            log.Information("Logged on user {@User}", user);
+        }
+
+        private static void StaticSerilog()
+        {
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+            Log.Information("Hello again, Serilog!");
+            var itemNumber = 10;
+            var itemCount = 999;
+            Log.Information("Processing item {ItemNumber} of {ItemCount}", itemNumber, itemCount);
+            Log.CloseAndFlush();
+        }
+    }
+}
---
+++ SerilogToFileConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using Serilog;
+using Serilog.Formatting.Compact;
+
+namespace SerilogToFileConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitialSerilog();
+        }
+
+        private static void InitialSerilog()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File(new CompactJsonFormatter(), "log.clef")
+                .CreateLogger();
+
+            var itemCount = 99;
+            for (var itemNumber = 0; itemNumber < itemCount; ++itemNumber)
+                Log.Debug("Processing item {ItemNumber} of {ItemCount}", itemNumber, itemCount);
+
+            Log.CloseAndFlush();
+        }
+    }
+}
